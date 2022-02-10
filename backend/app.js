82138e9eb171dc88da49d62e620aa4d67cc13e72
Marion COUTON <shaines.jm@gmail.com>
2022-02-10 15:09:32 +0100

    ajout erreur de CORS + intallation mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !'})
-})
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://marion44240:Jayan@cluster0.sweev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 module.exports = app;
